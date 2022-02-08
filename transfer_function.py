581c40bfbcad6dd37d057f643aee2b75952bdc1f
Prakrut Chaubal <prakrutchaubal@gmail.com>
2022-02-08 13:54:27 +1100

    minor

--- transfer_function.py
+++ transfer_function.py
@@ -10,21 +10,16 @@ def initial_estimate(cl_MC, cl_theory, fsky_w2,  delta_ell=1,   ):
     F0 = <C_l>_MC / (fsky * w2 * B_l^2 * C_th)
     """
     # The calculation is done in following steps:
-    # 1. Calculate the numerator. Smooth it at some delta ell. Default is delta ell = 50
-    # 2. Calculate the denominator
-    #   2.1. Multiply the theory Cls with the beam b_l**2 and set the negative values to zero
-    #   2.2. Calculate the initial estimate F0
+    # 1. Calculate the denominator
+    #   1.1. Multiply the theory Cls with the beam b_l**2 and set the negative values to zero
+    #   1.2. Calculate the initial estimate F0
     #-----------------
     # 1.
-    smooth_cl_MC = smooth( cl_MC , delta_ell )
-    # 2.
-    # 2.1
+    # 1.1
     cl_theory_beam2 = cl_theory * bl**2. # cl_theory_beam is just a moniker for Cl_theory with beam smoothing
-    neg_ind = np.where(cl_theory_beam2 < 0.)
-    #cl_theory_beam2[neg_ind] = 1. # not setting it to 0. to avoid zero division error in the next step
-    # 2.2
+    # 1.2
     F0 = smooth_cl_MC / ( fsky_w2 * cl_theory_beam2 )
-    F0[neg_ind] = 1.
+    F0[np.where(cl_theory_beam2 < 0.)] = 1.
     return F0
 
 def iteration( F0, M_ll, bl, fsky_w2, cl_theory, delta_ell ):
@@ -45,10 +40,3 @@ def calculate_tf():
     F = iteration(F0)
     return F
 
-def read_beams_file_and_interpolate( beams_file ):
-    # read the file
-    beams = np.loadtxt( beams_file )
-    beams_interp = np.interp( ell, beams[:,0], beams[:,1] )
-    # interpolate the beams
-    # return the interpolated beams
-    return beams
