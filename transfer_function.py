0119b842cd4bac32a4e27a3429f1ced46c1ee99b
Christian Reichardt <christian.reichardt@unimelb.edu.au>
2022-02-10 18:23:00 +1100

    comments and small fixes

--- transfer_function.py
+++ transfer_function.py
@@ -2,12 +2,20 @@ import numpy as np
 
 
 
-def initial_estimate(cl_MC, cl_theory, fsky_w2,  delta_ell=1,   ):
+def initial_estimate(cl_mc, cl_theory, fsky_w2):
     """
     Calculates the initial estimates of the transfer function for a MASTER style pipeline
     Implements the definition of F0 above eq 18 in MASTER(astro-ph/0105302)
     This definition is :
     F0 = <C_l>_MC / (fsky * w2 * B_l^2 * C_th)
+    
+    Inputs:
+    bl = beam function used in sims (N-vector)
+    cl_theory = power spectrum used to create sims, can be Dl or Cl - must be same as cl_mc (N-vector) 
+    cl_mc = power spectrum of processed sims, can be Dl or Cl - must be same as cl_theory (N-vector) 
+    fsky_w2 = float (fsky_w2 factor from master for normalization of mask)
+    Output: 
+    Returns: initial estimate of transfer function (N-vector)
     """
     # The calculation is done in following steps:
     # 1. Calculate the denominator
@@ -18,13 +26,27 @@ def initial_estimate(cl_MC, cl_theory, fsky_w2,  delta_ell=1,   ):
     # 1.1
     cl_theory_beam2 = cl_theory * bl**2. # cl_theory_beam is just a moniker for Cl_theory with beam smoothing
     # 1.2
-    F0 = smooth_cl_MC / ( fsky_w2 * cl_theory_beam2 )
+    F0 = cl_mc / ( fsky_w2 * cl_theory_beam2 )
     F0[np.where(cl_theory_beam2 < 0.)] = 1.
     return F0
 
-def iteration( F0, M_ll, bl, fsky_w2, cl_theory, delta_ell ):
-    cl_beam2 = cl_theory * bl**2
-    F = F0 + (cl_MC -  M_ll * (F0 * cl_beam2 ) ) / (cl_beam2 * fsky_w2)
+def iteration( F0, M_ll, fsky_w2, bl, cl_theory, cl_mc):
+    '''
+    Calculates the iterative3 estimates of the transfer function for a MASTER style pipeline (astro-ph/0105302)
+    
+    NB: all of the N-vectors below must share the same underlying ell-binning. No checks are made that they are consistent. 
+    Inputs:
+    F0 = previous transfer function iteration (N-vector)
+    M_ll = mode-coupling matrix (NxN array)
+    bl = beam function used in sims (N-vector)
+    cl_theory = power spectrum used to create sims, can be Dl or Cl - must be same as cl_mc (N-vector) 
+    cl_mc = power spectrum of processed sims, can be Dl or Cl - must be same as cl_theory (N-vector)
+    fsky_w2 = float (fsky_w2 factor from master for normalization of mask)
+    Outputs: 
+    Returns: next iteration of transfer function (N-vector)
+    '''
+    cl_theory_beam2 = cl_theory * bl**2
+    F = F0 + (cl_mc -  M_ll * (F0 * cl_theory_beam2 ) ) / (cl_theory_beam2 * fsky_w2)
     F[np.where(cl_beam2 < 0.)] = 1.
     return F
 
