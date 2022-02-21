7f421c2700c159660ee0b5576ae10fca93360356
Christian Reichardt <christian.reichardt@unimelb.edu.au>
2022-02-21 17:54:57 +1100

    i

--- test_bundles_20220221.py
+++ test_bundles_20220221.py
@@ -30,7 +30,7 @@ def test_n_bundles():
 
 
     banddef = np.arange(0,11000+1,50)
-    [0,500,1000,1500,2000,2200,2500,2800,3100,3500,4000,4600,5200,6000,6800,7800,9000,11
+    #[0,500,1000,1500,2000,2200,2500,2800,3100,3500,4000,4600,5200,6000,6800,7800,9000,11
     mcdir = workdir + '/mc/'
     mc_specrum = spec.unbiased_multispec(mapfiles, window, banddef, nside,
                                          lmax=lmax,
