d5ffeccf1211ef7af8f19244c23f990daa9f6dcc
Christian Reichardt <creichardt@scott.grid.uchicago.edu>
2022-02-23 06:28:22 -0600

    i

--- test_bundles_20220221.py
+++ test_bundles_20220221.py
@@ -1,24 +1,7 @@
-#import unbiased_multispec as um
+import unbiased_multispec as spec
 import numpy as np
 from spt3g import core,maps, calibration
 
-def sht_bundles():
-    mask = np.fromfile('/home/pc/hiell/mapcuts/apodization/apod_mask.npy',dtype=np.float32)
-    nmap=200
-    nside=8192
-    lmax=100 # test
-    file_list = ['/sptlocal/user/pc/sim_coadds/bundle{:03d}_150GHz.g3.gz'.format(i) for i in range(nmap)]
-    processed_sht_file = '/sptlocal/user/creichardt/hiell2022/sht_sim1_lmax100.bin'
-    take_and_reformat_shts(file_list, processed_sht_file,
-                           nside,lmax,
-                           cmbweighting = True, 
-                           mask  = mask,
-                           kmask = None,
-                           ell_reordering=None,
-                           no_reorder=False,
-                           ram_limit = None,
-                           npmapformat=False
-                          )
 
 
 def combine_lr_bundles():
@@ -57,6 +40,25 @@ def test_n_bundles():
                                          kmask=None,
                                          cmbweighting=True)
 
+
+
+def sht_bundles():
+    mask = np.fromfile('/home/pc/hiell/mapcuts/apodization/apod_mask.npy',dtype=np.float32)
+    nmap=200
+    nside=8192
+    lmax=100 # test
+    file_list = ['/sptlocal/user/pc/sim_coadds/bundle{:03d}_150GHz.g3.gz'.format(i) for i in range(nmap)]
+    processed_sht_file = '/sptlocal/user/creichardt/hiell2022/sht_sim1_lmax100.bin'
+    spec.take_and_reformat_shts(file_list, processed_sht_file,
+                           nside,lmax,
+                           cmbweighting = True, 
+                           mask  = mask,
+                           kmask = None,
+                           ell_reordering=None,
+                           no_reorder=False,
+                           ram_limit = None,
+                           npmapformat=False
+                          )
 
 
 if __name__ == "__main__":
