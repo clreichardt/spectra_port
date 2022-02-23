4e1128b803e281dab5b9be653206de989e44f149
Christian Reichardt <christian.reichardt@unimelb.edu.au>
2022-02-23 22:40:02 +1100

    i test sht

--- test_bundles_20220221.py
+++ test_bundles_20220221.py
@@ -2,7 +2,23 @@
 import numpy as np
 from spt3g import core,maps, calibration
 
-
+def sht_bundles():
+    mask = np.fromfile('/home/pc/hiell/mapcuts/apodization/apod_mask.npy',dtype=np.float32)
+    nmap=200
+    nside=8192
+    lmax=100 # test
+    file_list = ['/sptlocal/user/pc/sim_coadds/bundle{:03d}_150GHz.g3.gz'.format(i) for i in range(nmap)]
+    processed_sht_file = '/sptlocal/user/creichardt/hiell2022/sht_sim1_lmax100.bin'
+    take_and_reformat_shts(file_list, processed_sht_file,
+                           nside,lmax,
+                           cmbweighting = True, 
+                           mask  = mask,
+                           kmask = None,
+                           ell_reordering=None,
+                           no_reorder=False,
+                           ram_limit = None,
+                           npmapformat=False
+                          )
 
 
 def combine_lr_bundles():
@@ -44,5 +60,6 @@ def test_n_bundles():
 
 
 if __name__ == "__main__":
-    combine_lr_bundles()
+    sht_bundles()
+    #combine_lr_bundles()
     #test_n_bundles()
