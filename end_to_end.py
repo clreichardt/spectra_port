8b3f37341661b0617f3299f29fd309eb83bf7828
Christian Reichardt <christian.reichardt@unimelb.edu.au>
2022-03-10 11:02:24 +1100

    output tracking

--- end_to_end.py
+++ end_to_end.py
@@ -8,48 +8,89 @@ if __name__ == "__main__":
     mapfiles = np.zeros([3,4])
     setdef_mc = np.zeros(100)
     setdef = np.zeros(100)
+    workdir = '/scratch/cr/xspec_2022/'
+
+    mask = utils.load_window(mask_file)
     
-    end_to_end(
-        mapfiles,
-        mcmapfiles,
-        beamfiles,
-        simbeamfiles=None,
-        setdef=setdef,
-        setdef_mc=setdef_mc,
-        do_window_func=False, 
-        banddef = banddef,
-        lmax=13000,
-        nside=8192,
-        window_file='Placeholder',
-        kernel_file ='Placeholder',
-        mapfiles=mapfiles,
-        setdef_mc = setdef_mc,
-        
-        
-    )
+    output = end_to_end( mapfiles,
+                         mcmapfiles,
+                         banddef,
+                         beamfiles,
+                         workdir,
+                         simbeamfiles=None,
+                         setdef=setdef,
+                         setdef_mc=setdef_mc,
+                         do_window_func=False, 
+                         banddef = banddef,
+                         lmax=13000,
+                         nside=8192,
+                         kmask=None,
+                         mask=mask,
+                         kernel_file ='Placeholder',
+                         resume-True
+                       )
+    with open(file_out,'wb') as fp:
+        pkl.save(output,fp)
     
     
     
-def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamfiles=None,do_window_func = True,lmax=13000,nside=8192,window_file=None,kernel_file=None):    
+    
+def end_to_end(mapfiles,
+               mcmapfiles,
+               banddef,
+               beamfiles,
+               workdir,
+               setdef=None,
+               setdef_mc=None,
+               simbeamfiles=None,
+               do_window_func = True,
+               lmax=13000,
+               nside=8192,
+               kmask=None,
+               mask=None,
+               kernel_file=None,
+               resume=True):    
     '''
     should add argument to this...
     '''
-    real_map_dir = ''
-    real_map_stub= ''
 
+    #storage dictionary for later monitoring
+    output = {}
+    output['mask']=mask
+    output['kmask']=kmask
+    output['lmax']=lmax
+    output['nside']=nside
+    output['resume']=resume
+    output['mapfiles']=mapfiles
+    output['mcmapfiles']=mcmapfiles
+    output['banddef']=banddef
+    output['beamfiles']=beamfiles
+    output['simbeamfiles']=simbeamfiles
+    output['kernel_file']=kernel_file
+    output['workdir']=workdir
+    output['do_window_func']=do_window_func
+    
     ##################
     # 1: Make (or load) mode coupling kernel
     ##################
-    window = utils.load_window(window_file)
 
+
+    
     # should have already done a binned version of the M_ll
     # also need to have move it to Dl space 
     # this code is here, but commented out
     #
     
     #plan to use wrapper to NaMaster, to be written
-    info, mll = utils.load_mll(kernel_file)
+    info, kernel = utils.load_mll(kernel_file)
     banddef_fine = utils.bands_from_range(info)
+    ellkern = utils.band_centers(banddef_fine)
+
+    #and store
+    output['info']=info
+    output['kernel']=kernel
+    output['ellkern']=ellkern
+    output['banddef_fine']=banddef_fine
 
     ##################
     # 2: Calculate spectra and covariances of monte-carlo sims
@@ -57,35 +98,45 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
     ##################
 
     mcdir = workdir + '/mc/'
+
     # will used alm's in mcdir+'shts_processed.bin'
-    mc_specrum = spec.unbiased_multispec(mapfiles,window,banddef,nside,lmax=lmax,
-                                         resume=resume,
-                                         basedir=mcdir,
-                                         setdef=setdef_mc,
-                                         jackknife=False, auto=False,
-                                         kmask=None,
-                                         cmbweighting=True)
-    mc_specrum_fine = spec.unbiased_multispec(mapfiles,window,banddef_fine,nside,lmax=lmax,
+    mc_specrum      = spec.unbiased_multispec(mcmapfiles,mask,banddef,nside,
+                                              lmax=lmax,
+                                              resume=resume,
+                                              basedir=mcdir,
+                                              setdef=setdef_mc,
+                                              jackknife=False, auto=False,
+                                              kmask=kmask,
+                                              cmbweighting=True)
+    
+    mc_specrum_fine = spec.unbiased_multispec(mcmapfiles,mask,banddef_fine,nside,
+                                              lmax=lmax,
                                               resume=True, #reuse the SHTs
                                               basedir=mcdir,
                                               setdef=setdef_mc,
                                               jackknife=False, auto=False,
-                                              kmask=None,
+                                              kmask=kmask,
                                               skipcov=True,
                                               cmbweighting=True)
+
+    output['mc_spectrum']=mc_spectrum
+    output['mc_spectrum_fine']=mc_spectrum_fine
 
     ##################
     # 3: Calculate spectra and covariances of data
     ##################
     datadir = workdir + '/data/'
-    data_specrum = spec.unbiased_multispec(mapfiles,window,banddef,nside,lmax=lmax,
-                                      resume=resume,
-                                      basedir=datadir,
-                                      setdef=setdef,
-                                      jackknife=False, auto=True,
-                                      kmask=None,
-                                      cmbweighting=True)
+    data_specrum    = spec.unbiased_multispec(mapfiles,mask,banddef,nside,
+                                              lmax=lmax,
+                                              resume=resume,
+                                              basedir=datadir,
+                                              setdef=setdef,
+                                              jackknife=False, auto=False,
+                                              kmask=kmask,
+                                              cmbweighting=True)
 
+    output['data_spectrum']=data_spectrum
+    
     ##################
     # 4: Calculate the Transfer functions
     ##################
@@ -103,6 +154,11 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
         assert(len(simbeamfiles) == nsets)
         simbeams_interp = utils.fill_in_beams(simbeamfiles,ellkern)
         simbeams = utils.explode_beams(simbeams_interp)
+    output['beams']=beams
+    output['beams_interp']=beams_interp
+    output['simbeams']=simbeams
+    output['simbeams_interp']=simbeams_interp
+        
     #see transfer_function.py
 
     '''
@@ -115,15 +171,15 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
     beams_for_tf=simbeam_interp
     ntfs=nsets #  this might be changed below
 
-'''
-; This is where we need to know the theory spectrum of the monte carlo sims
-; The user has the option of inputting one file per set OR
-;  a list of files or "components" per set. 
-; Thirdly, if the user specifies one component per spectrum (including 
-; cross spectra), then a separate transfer function will be made for
-; the cross spectra, (rather than using the geometric mean of the
-; the single frequency spectra)
-'''
+    '''
+    ; This is where we need to know the theory spectrum of the monte carlo sims
+    ; The user has the option of inputting one file per set OR
+    ;  a list of files or "components" per set. 
+    ; Thirdly, if the user specifies one component per spectrum (including 
+    ; cross spectra), then a separate transfer function will be made for
+    ; the cross spectra, (rather than using the geometric mean of the
+    ; the single frequency spectra)
+    '''
 
     ##################
     # 5: Create binned kernel, including mode-coupling, beams, pixel WFs, transfer functions
@@ -137,8 +193,8 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
     inv_sim_super_kernel= np.zeros([nspectra, nbands, nspectra, nbands],dtype=np.float64)
     iskips = np.zeros(nspectra, dtype=np.int32)
     for i in range(nspectra):
-        super_kernel[i,:,i,:]=rebin_coupling_kernel(kernel, ellkern, banddef, transfer=transfer[i,:], beam = beams[i,:])
-        sim_super_kernel[i,:,i,:]=rebin_coupling_kernel(kernel, ellkern, banddef, transfer=transfer[i,:], beam = simbeams[i,:])
+        super_kernel[i,:,i,:]     = rebin_coupling_kernel(kernel, ellkern, banddef, transfer=transfer[i,:], beam = beams[i,:])
+        sim_super_kernel[i,:,i,:] = rebin_coupling_kernel(kernel, ellkern, banddef, transfer=transfer[i,:], beam = simbeams[i,:])
         slice = [superkern[i,j,i,j]==0 for j in range(nbands)]
         try:
             iskip = np.where(slice)[-1][-1]
@@ -149,27 +205,40 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
         inv_super_kernel[i,iskip:,i,iskip:] = np.linalg.inv(super_kernel[i,iskip:,i,iskip:])
         inv_sim_super_kernel[i,iskip:,i,iskip:] = np.linalg.inv(sim_super_kernel[i,iskip:,i,iskip:])
         
-    invkernmat = np.reshape(inv_super_kernel,[nspectra*nbands, nbands*nspectra]))
-    invkernmattr = np.transpose(invkernmat)
-    invsimkernmat = np.reshape(inv_sim_super_kernel,[nspectra*nbands, nbands*nspectra]))
+    invkernmat      = np.reshape(inv_super_kernel,[nspectra*nbands, nbands*nspectra]))
+    invkernmattr    = np.transpose(invkernmat)
+    invsimkernmat   = np.reshape(inv_sim_super_kernel,[nspectra*nbands, nbands*nspectra]))
     invsimkernmattr = np.transpose(invsimkernmat)
 
+    output['super_kernel']=super_kernel
+    output['sim_super_kernel']=sim_super_kernel
+    output['inv_super_kernel']=inv_super_kernel
+    output['inv_sim_super_kernel']=inv_sim_super_kernel
+    output['invkernmat']=invkernmat
+    output['invkernmatt']=invkernmattr
+    output['invsimkernmat']=invsimkernmat
+    output['invsimkernmatt']=invsimkernmattr
+                
     ##################
     # 6: Multiply data bandpowers by Inverse Kernel
     ##################
     spectrum = np.reshape(invkernmat * data_spectrum.spectrum,[nspectra,nbands])
 
+    output['spectrum']=spectrum
     ##################
     # 7: Apply inverse kernel to the covariance matrices
     ##################
     sample_cov = np.reshape(invsimkernmat * mc_spectrum.cov * invsimkernmattr,[nspectra,nbands,nspectra, nbands])
     meas_cov = np.reshape(invkernmat * data_spectrum.cov * invkernmattr,[nspectra,nbands,nspectra, nbands])
-    
+
+    output['sample_cov']=sample_cov
+    output['meas_cov']=meas_cov
     ##################
     # 8: Combine covariances yield total covariance estimate
     ##################
     cov = meas_cov + sample_cov
 
+    output['cov']=cov
     ##################
     # 9: Optionally: Calculate bandpower window functions
     ##################
@@ -186,3 +255,9 @@ def end_to_end(mapfiles,mcmapfiles,beamfiles,setdef=None,setdef_mc=None,simbeamf
                         'bl':beams[i,:]}
             windowfunc[iskip+i*nbands:nbands+i*nbands,:] = 
                 window_function_calc(banddef,transdic,nskip=iskip,ellmin = win_minell,ellmax=win_maxell)
+
+
+    output['windowfunc']=windowfunc
+
+    
+    return output
