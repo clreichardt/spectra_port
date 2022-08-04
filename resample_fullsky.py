282bc186d896df5b73303bea6750e8218b315d27
Christian Reichardt <christian.reichardt@unimelb.edu.au>
2022-08-05 09:07:09 +1000

    rerun

--- resample_fullsky.py
+++ resample_fullsky.py
@@ -8,23 +8,95 @@ from spectra_port import unbiased_multispec as spec
 import pickle as pkl
 
 
+def get_indices():
+    ind,map = spec.load_spt3g_healpix_ring_map('/sptlocal/user/pc/g3files_v2/combined_T_148ghz_00024.g3')
 
-ind,map = spec.load_spt3g_healpix_ring_map('/sptlocal/user/pc/g3files_v2/combined_T_148ghz_00024.g3')
-dir = '/big_scratch/cr/mmdl/'
 
+    theta,phi = hp.pixelfunc.pix2ang(nside,ind)
+
+
+    thetamult = [-1,1,-1]
+    thetaoff = np.pi * np.asarray([1.,0,1.])
+    phioff = np.pi * np.asarray([0,1.,1.])
+    i=0
+    newtheta = thetamult[i] * theta + thetaoff[i]
+    newphi = phi + phioff[i]
+    newphi[newphi > 2*np.pi] -= 2*np.pi
+    ind1 = hp.ang2pix(nside,newtheta,newphi)
+    i=1
+    newtheta = thetamult[i] * theta + thetaoff[i]
+    newphi = phi + phioff[i]
+    newphi[newphi > 2*np.pi] -= 2*np.pi
+    ind2 = hp.ang2pix(nside,newtheta,newphi)
+    i=2
+    newtheta = thetamult[i] * theta + thetaoff[i]
+    newphi = phi + phioff[i]
+    newphi[newphi > 2*np.pi] -= 2*np.pi
+    ind3 = hp.ang2pix(nside,newtheta,newphi)
+
+    return ind, ind1, ind2, ind3
+
+
+
+fullmap = hp.read_map('/sptlocal/user/creichardt/out_maps/xfer/sim_lmax15000_nside8192_interp3.0_method1_1_seed5929_fgseed001627_radioseed012517_150ghz_map.fits')
 #mask_file='/home/pc/hiell/mapcuts/apodization/apod_mask.npy'
 #mask = np.load(mask_file)
 print(ind.shape)
 nside=8192
-theta,phi = hp.pixelfunc.pix2ang(nside,ind)
+
+# Convert numpy arrays from nersc to g3 healpix format and save to g3
+def save_as_g3(cutsky, ind, file_name):
+    #
+    #cutsky *= core.G3Units.uK
+    cutsky = cutsky.astype(dtype=np.float64)
+    store = ind, cutsky, 8192
+    m = maps.HealpixSkyMap(store)
+    
+    #create a g3 frame to store the date
+    frame = core.G3Frame(core.G3FrameType.Map)
+    m.ringparse = True
+    frame['T'] = m
+    writer = core.G3Writer(file_name)
+    writer(frame)
+    return
 
 
-thetamult = [-1,1,-1]
-thetaoff = np.pi * np.asarray([1.,0,1.])
-phioff = np.pi * np.asarray([0,1.,1.])
-for i in range(3):
-    newtheta = thetamult[i] * theta + thetaoff[i]
-    newphi = phi + phioff[i]
-    newphi[newphi > 2*np.pi] -= 2*np.pi
-    newind = hp.ang2pix(nside,newtheta,newphi)
-    print(newind.shape)
+def loop_and_cut(ostub='/sptlocal/user/creichardt/out_maps/sim_150ghz_{}.g3',file_list):
+
+    nf = len(file_list)
+    ind0,ind1,ind2,ind3 = get_indices()
+    i=0
+    for j in range(nf):
+        file=file_list[j]
+        fullmap = hp.read_map(file)
+        cutsky = fullmap[ind0]
+        file_name=ostub.format(i)
+        save_as_g3(cutsky, ind0, file_name)
+        i=i+1
+        cutsky = fullmap[ind1]
+        file_name=ostub.format(i)
+        save_as_g3(cutsky, ind0, file_name)
+        i=i+1
+        cutsky = fullmap[ind2]
+        file_name=ostub.format(i)
+        save_as_g3(cutsky, ind0, file_name)
+        i=i+1
+        cutsky = fullmap[ind3]
+        file_name=ostub.format(i)
+        save_as_g3(cutsky, ind0, file_name)
+        i=i+1
+
+
+def do_all():
+    stub='sim*150ghz_map.dat'
+    dir='/sptlocal/user/creichardt/out_maps/xfer/'
+    file_list = glob.glob(dir+stub)
+
+    loop_and_cut(ostub='/sptlocal/user/creichardt/out_maps/sim_150ghz_{}.g3',file_list)
+    file_list2=[file.replace('150ghz_map.dat','220ghz_map.dat') for file in file_list]
+    loop_and_cut(ostub='/sptlocal/user/creichardt/out_maps/sim_220ghz_{}.g3',file_list2)
+    file_list3=[file.replace('150ghz_map.dat','95ghz_map.dat') for file in file_list]
+    loop_and_cut(ostub='/sptlocal/user/creichardt/out_maps/sim_95ghz_{}.g3',file_list3)
+    
+if __name__ == "__main__":
+    do_all()
